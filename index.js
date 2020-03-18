8c8f123a86f22fd03e4ef08d267109067cec93f1
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 04:05:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ function fetchBooks() {
   .then(function(response) {
     return response.json();
   })
-  .then(function(json) {
-    return json;
-});
+return fetch()
 }
 
 function renderBooks(json) {
